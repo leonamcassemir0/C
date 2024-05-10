73ab48cdacd39687807d67ca017f3057d7fd42ee
leonamcassemir0 <114764927+leonamcassemir0@users.noreply.github.com>
2024-05-09 21:53:21 -0300

    comentarios

---
+++ identaçao_comentarios.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    /*
+        1) Instruções
+        2) endentação
+        3) Comentário C
+    */
+
+    /* Comentários de várias linhas*/
+    // Comentário de uma linha
+
+    printf("");
+    return 0;
+}
