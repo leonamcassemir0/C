13dc117d35703531b5c96c6999d7f5cc4284ff59
leonamcassemir0 <114764927+leonamcassemir0@users.noreply.github.com>
2024-05-09 22:34:07 -0300

    x

---
+++ variaveis3.c
@@ -0,0 +1,11 @@
+/*
+    Tipos de variavéis:
+        1) char (caractere) ocupa 1 byte, ou seja, 8 bits.
+        2) int (inteiro) ocupa 2 bytes
+            - Vai de -32.768 até +32767
+        3) float (flutuante) ocupa 4 bytes
+        4) void (vazio) não ocupa espaço.
+        5) double é usado para números com mais casas decimais, afim de ter mais precisão.
+            - Ocupa 8 bytes.
+
+*/
