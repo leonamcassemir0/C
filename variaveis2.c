7c43bc189f7dd8e7095d5e9dea61009d586153ef
leonamcassemir0 <114764927+leonamcassemir0@users.noreply.github.com>
2024-05-09 22:25:43 -0300

    criei variaveis2

---
+++ variaveis2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num = 10, num2 = 2;
+    
+    // Printing num and num2 separately
+    printf("%d\n", num);
+    printf("%d\n", num2);
+    
+    // Printing numeros_2
+    int numeros_2 = 23;
+    printf("%d\n", numeros_2);
+    
+    return 0;
+}
